168dd8b946fed2f4ecc20e754fdb7a87507b0aab
S2 support team access <s2support@Jenkins.(none)>
2013-10-25 09:17:54 -0400

    initial commit

---
+++ Linux_i686/toolchain_armv5/lib/gcc/armv5-s2-linux-gnueabi/4.6.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.6.1/configure --target=armv5-s2-linux-gnueabi --prefix=/home/lux/.rim/tools/toolchain_armv5 --enable-interwork --disable-multilib --enable-languages=c,c++ --with-sysroot=/home/lux/.rim/roots/Linux/Fedora/15/armv5 --with-gmp-include=/home/lux/.rim/tools/build/gmp --with-gmp-lib=/home/lux/.rim/tools/build/gmp/.libs --with-arch=armv5 --enable-__cxa_atexit --without-headers --disable-shared --disable-nls --with-gnu-as --with-gnu-ld";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv5" } };
